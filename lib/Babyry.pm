cae9ea6048cdbad9372334ef145c1c029614ed79
babyry <babyry@example.com>
2014-01-11 20:08:43 +0900

    Fix log

--- lib/Babyry.pm
+++ lib/Babyry.pm
@@ -7,22 +7,26 @@ use 5.008001;
 use Sys::Hostname qw/hostname/;
 use File::Stamped;
 use Carp;
+use Encode;
+use Log::Minimal;
 
 use parent qw/Amon2/;
+
 # Enable project local mode.
-__PACKAGE__->make_local_context();
+#__PACKAGE__->make_local_context();
 
 {
     my $base_dir = __PACKAGE__->base_dir;
-    my $fh = File::Stamped->new(pattern => $base_dir . "/log/world.log.%Y%m%d");
+    my $fh = File::Stamped->new(pattern => $base_dir . "/log/babyry.log.%Y%m%d");
     my $hostname = hostname;
     if ( ! $ENV{DEBUG} ) {
         $Log::Minimal::PRINT = sub {
             my ($time, $type, $message, $trace) = @_;
             print {$fh} decode_utf8(sprintf("(%s) %s [%s] %s at %s\n", $hostname, $time, $type, $message, $trace));
-        }
+        };
     }
 }
+
 
 
 1;
