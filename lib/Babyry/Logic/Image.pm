2fb5993150a99a7550a1e4274ad7fce60a7e67df
babyry <babyry@example.com>
2014-01-12 22:36:45 +0900

    create image api

--- lib/Babyry/Logic/Image.pm
+++ lib/Babyry/Logic/Image.pm
@@ -14,6 +14,8 @@ use parent qw/Babyry::Logic/;
 
 use SQL::Abstract;
 
+use Babyry::Logic::Sequence;
+
 my $ACCESS_KEY = $ENV{AWS_SECRET_ACCESS_KEY};
 my $KEY_ID = $ENV{AWS_ACCESS_KEY_ID};    
 
@@ -28,7 +30,7 @@ sub create_signature {
             { bucket => $bucket },
             [ "starts-with", "\$key", "test/" ],
             { "acl" => "private" },
-            { "success_action_redirect" => "http://babyryserver5001/image/get_url" },
+            { "success_action_redirect" => "http://babyryserver5001/image/redirect" },
             [ "starts-with", "\$Content-Type", "" ],
             [ 'content-length-range', 0, 1073741824 ],
         ],
@@ -51,6 +53,32 @@ sub create_signature {
         signature => $signature,
     };
     return $res;
+}
+
+sub set_image_id {
+    my ($self, $bucket, $key) = @_;
+
+    my $sequence = Babyry::Logic::Sequence->new();
+    my $image_id = $sequence->get_id('seq_image');
+
+    # TODO
+    my $user_id = '1';
+
+    my $dbh = $self->dbh('TEST_W');
+    my $sth = $dbh->prepare("insert into image (image_id, user_id, bucket, image_key, created_at) values (?,?,?,?,?)");
+    $sth->execute($image_id, $user_id, $bucket, $key, time());
+    $dbh->commit;
+}
+
+sub get_image_url {
+    my ($self, $bucket, $key) = @_;
+
+    my $home_dir = Babyry->base_dir;
+    my $ruby = "/home/babyry/.rbenv/shims/ruby $home_dir/lib/Babyry/Logic/get_onetime_url.rb";
+    my $url = `$ruby $bucket $key`;
+    chomp($url);
+
+    return $url;
 }
 
 1;
