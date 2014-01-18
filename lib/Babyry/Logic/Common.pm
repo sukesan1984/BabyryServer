08a2d5a235e49e39d3c212a0d0a86a5d1eb29cd7
hirata-motoi <hirata.motoi@gmail.com>
2014-01-18 17:16:54 +0900

    Adopt config modification

--- lib/Babyry/Logic/Common.pm
+++ lib/Babyry/Logic/Common.pm
@@ -6,12 +6,13 @@ use warnings;
 
 use parent qw/Babyry/;
 use Log::Minimal;
+use Babyry::Common;
 use Digest::SHA qw/hmac_sha256_hex/;
 
 # TODO implement more strictly
 sub enc_password {
     my ($self, $password) = @_;
-    my $secret = __PACKAGE__->config->{'register_secret'};
+    my $secret = Babyry::Common->get_key_vault('register_secret');
     return hmac_sha256_hex($password . $secret);
 }
 
